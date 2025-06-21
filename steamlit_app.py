22698e3f20ff29d8eb969a0727a3bdf245545b65
manpat2201 <patil.manjushree@gmail.com>
2025-06-21 09:31:30 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -78,7 +78,7 @@ if(ingredients_list):
       ingredients_string += fruit_chosen + ' '
       
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + ' search_on')
-      st.write('Manju')
+      st.write(search_on)
       pd_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
 

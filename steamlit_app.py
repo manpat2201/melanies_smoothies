da36a52cc27c3b9f24d176a0773707e36635a651
manpat2201 <patil.manjushree@gmail.com>
2025-06-21 09:19:44 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -66,7 +66,7 @@ if(ingredients_list):
 
   for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '  
-    #st.dataframe(pd_df)
+    st.dataframe(pd_df)
     #st.stop()
 
     search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]

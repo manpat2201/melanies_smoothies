a3dde66147a16a364037c332762ceb8d067aea5b
manpat2201 <patil.manjushree@gmail.com>
2025-06-21 08:02:51 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -58,9 +58,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 if(ingredients_list):     
-    ingredients_string = ''
+  ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
+  for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
